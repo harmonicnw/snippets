ac91897f6d3477b6bf918c659a29d9fa4c0e068a
thomasbruketta <thomasbruketta@gmail.com>
2013-12-09 17:47:09 -0800

    Added snippets that will preload images
    Snippet intended to remove lag experienced when images are called after
    a page loads, such as with a hover state.

---
+++ js/jquery/preload/jquery.hmc-preload.js
@@ -0,0 +1,48 @@
+(function ( $ ) {
+
+	/*
+	Harmonic Pre-loader jQuery plugin
+	Version: 1.0.0
+	
+	Dependencies:
+	 - jQuery (optimized for 10.2)
+	
+	Options:
+	{
+		images: ( required | arr  ) an array of image sources to be preloaded
+	}
+	
+	Usage:
+	$('body').hmcPreload({
+		images : [
+		'http://www.awesomestuff.com/coolman.png',
+		'http://www.dinostuff.com/trex.jpeg',
+		'http://www.lordoftheringstrivia.com/onering.jpeg'
+		]
+	});
+	
+	To do:
+	 - 
+	*/
+
+	// preload images
+
+	$.fn.hmcPreload = function( optionsPassed ) {
+		var options = {
+			images: []
+		}
+		
+		$.extend( options, optionsPassed );
+		
+		if ( $("#preloadImages").length <= 0 ) {
+			$(this).append("<div id='preloadImages' style='display:none' />");
+		}
+		
+		var p = $("#preloadImages");
+		
+		for (var i = 0; i < options.images.length; i++) {
+			p.append( "<img src='" + options.images[i] + "' />" );
+		}
+	}
+
+} ( jQuery ));
