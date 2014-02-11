9d5899f4aad9f276d86b9b94562cda63f4ac11b7
Thomas Bruketta <thomas@harmonicnw.com>
2014-02-11 13:32:50 -0800

    Added ability to set different buffer tops for mobile devices that may not have a fixed nav

--- js/jquery/animate-to-anchor/jquery.hmc-animate-to-anchor.js
+++ js/jquery/animate-to-anchor/jquery.hmc-animate-to-anchor.js
@@ -2,44 +2,53 @@
 
 	/*
 	Harmonic Animate to Anchor jQuery pluggin 
-	Version: 1.0.1
+	Version: 1.0.2
 	
 	Options:
 	{
 		bufferTop : ( optional | integer | default = 0 ) identifies the top buffer for pages that have a sticky header. 
+		bufferTopMobile : (optional | integer | default = 0) sets the buffer for mobile devices.
+		breakpoint: (optional | integer | deafault = 768) sets the breakpoint for mobile devices (in pixels)
 
 	}
 	
 	Usage:
 
-	$("nav").hmcAnimateToAnchor( {bufferTop : 40} );
+	$("nav").hmcAnimateToAnchor( {bufferTop : -40, bufferTopMobile : -10, breakpoint : 992} );
 	
 	To do:
 	*/
 	
 	// initialize animated scroll to in-page anchor link
 	$.fn.hmcAnimateToAnchor = function( optionsPassed ) {
-		
+
 		// set initial parameters
 		var options = {
-			bufferTop : 0
+			bufferTop : 0,
+			bufferTopMobile: 0,
+			breakpoint : 768, 
 		}
-		
+
 		$.extend( options, optionsPassed );
-		
+
 		// find all anchor tags with hashtag links
 		$(this).find('a[href^=#]').each( function() {
 			var target = $( $(this).attr('href') );
-			
+
 			if (target.length > 0) {
 				$(this).click( function(e) {
 					var targetOffset = target.offset();
+					if (window.outerWidth >= options.breakpoint){
 						var scrollTo = targetOffset.top + options.bufferTop;
-					
+					} 
+					else {
+						var scrollTo = targetOffset.top + options.bufferTopMobile;
+					}
+
 					$('body,html').animate({
 						'scrollTop' : scrollTo + 'px'
 					}, 1000);
-					
+
 					e.preventDefault();
 					$(this).blur();
 				});
