ba7de6a71162fff507e1d8a21ad5e0f00a3af509
Gage Choat <gagechoat@gmail.com>
2016-07-14 13:29:40 -0700

    fix jslink warning, reconfigure to use method notation

--- js/jquery/same-size/jquery.hmc-samesize.js
+++ js/jquery/same-size/jquery.hmc-samesize.js
@@ -2,78 +2,77 @@
 
 	/*
 	Harmonic Same Sizer jQuery plugin
-	Version: 1.1
-	
+	Version: 1.2
+
 	Options:
 	{
 		height : ( optional | boolean | default = true ) if true, heights will be set to match
 		images : ( optional | boolean | default = false ) if true, widths will be set to match
 		windowMinWidth : ( optional | number | default = 0 ) will only run resizer if window is at or over n pixels wide
-		windowMaxWidth : ( optional | number | default = 99999 ) will only run resizer if window is under n pixels wide 
+		windowMaxWidth : ( optional | number | default = 99999 ) will only run resizer if window is under n pixels wide
 	}
-	
+
 	Usage:
 	$('.some .columns').hmcSameSize();
-	
+
 	To do:
 	 - check for preset heights and widths directly on DOM objs, save into object data, use for resetting original height and width when over/under windowMinWidth/windowMaxWidth
 	 - add option to hide objects until script has run (along with some animation options like fade speed, easing, etc.)
 	 - add option to ensure that imgs within DOM object have loaded
 	*/
-	
-	$.fn.hmcSameSize = function( optionsPassed ) {
-		var targets = $(this);
 
-		// set initial parameters
-		var options = {
-			height : true,
-			width : false,
-			windowMinWidth : 0,
-			windowMaxWidth : 9999
-		}
-		
-		$.extend( options, optionsPassed );
-		
-		$(window).resize( function() {
-			targets.hmcSSGo( options );
-		});
-		
-		targets.hmcSSGo( options );
-	}
-	
-	$.fn.hmcSSGo = function( options ) {
-		$(this).css('height','auto');
-		// if window is within min/max width, resize objects, otherwise set to auto
-		if ( $(window).get( 0 ).outerWidth >= options.windowMinWidth && $(window).get( 0 ).outerWidth < options.windowMaxWidth ) {
-			
-			var biggestHeight = 0;
-			var biggestWidth = 0;
-			
-			// cycle through objects to find biggest heights and widths
-			$(this).each( function() {
-				if ( $(this).outerHeight() > biggestHeight ) {
-					biggestHeight = $(this).outerHeight();
-				}
-				if ( $(this).outerWidth() > biggestWidth ) {
-					biggestWidth = $(this).outerWidth();
-				}
+	$.fn.extend({
+		hmcSameSize: function( optionsPassed ) {
+			var targets = $(this);
+
+			// set initial parameters
+			var options = {
+				height : true,
+				width : false,
+				windowMinWidth : 0,
+				windowMaxWidth : 9999
+			};
+
+			$.extend( options, optionsPassed );
+
+			$(window).resize( function() {
+				targets.hmcSSGo( options );
 			});
-			
-			// set height and width of objects
-			if ( options.height ) {
-				$(this).outerHeight( biggestHeight );
-			}
-			if ( options.width ) {
-				$(this).outerWidth( biggestWidth );
-			}
-			
-		} else {
-		
-			// remove possibly pre-set heights and widths
-			$(this).css('height','').css('width','');
-			
-		}
-		
-	}
 
+			targets.hmcSSGo( options );
+		},
+		hmcSSGo: function( options ) {
+			$(this).css('height','auto');
+			// if window is within min/max width, resize objects, otherwise set to auto
+			if ( $(window).get( 0 ).outerWidth >= options.windowMinWidth && $(window).get( 0 ).outerWidth < options.windowMaxWidth ) {
+
+				var biggestHeight = 0;
+				var biggestWidth = 0;
+
+				// cycle through objects to find biggest heights and widths
+				$(this).each( function() {
+					if ( $(this).outerHeight() > biggestHeight ) {
+						biggestHeight = $(this).outerHeight();
+					}
+					if ( $(this).outerWidth() > biggestWidth ) {
+						biggestWidth = $(this).outerWidth();
+					}
+				});
+
+				// set height and width of objects
+				if ( options.height ) {
+					$(this).outerHeight( biggestHeight );
+				}
+				if ( options.width ) {
+					$(this).outerWidth( biggestWidth );
+				}
+
+			} else {
+
+				// remove possibly pre-set heights and widths
+				$(this).css('height','').css('width','');
+
+			}
+		}
+	});
 }( jQuery ));
