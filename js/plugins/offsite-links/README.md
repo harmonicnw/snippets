b9e2a47fbfad166f5e55c42f87a62ea6819dfd30
gagechoat <gagechoat@gmail.com>
2018-01-24 13:51:51 -0800

    add vanilla js offsite links plugin

---
+++ js/plugins/offsite-links/README.md
@@ -0,0 +1,19 @@
+# Harmonic Offsite Links jQuery plugin
+### Version: 1.1.0
+
+## Dependencies
+
+* none
+
+## Usage
+
+```
+$('body').hmcOffsiteLinks({
+	site : "harmonicnw.com"
+});
+```
+
+### Options
+
+* **site** : *( optional | string | default = document.location.host )* string that identifies this site
+* **target** : *( optional | string | default = 'body' )* selector of container element to apply offsite links to
---
+++ js/plugins/offsite-links/hmc-offsite-links.js
@@ -0,0 +1,52 @@
+var hmcOffsiteLinks;
+
+(function () {
+
+	/*
+	Harmonic Offsite Links Plugin
+	Version: 1.1.0
+	https://github.com/harmonicnw/snippets/tree/master/js/plugins/offsite-links
+
+	Dependencies:
+	 - none
+
+	Options:
+	{
+		site : ( optional | string | default = document.location.host ) string that identifies this site
+		target : ( optional | string | default = 'body' ) selector of container element to apply offsite links to
+	}
+
+	Usage:
+	hmcOffsiteLinks({
+		site : "harmonicnw.com"
+	});
+	*/
+
+	hmcOffsiteLinks = function( optionsPassed ) {
+
+		// set initial parameters
+		var options = {
+			site: document.location.host,
+			target: 'body'
+		};
+
+		// $.extend( options, optionsPassed );
+		if (optionsPassed) {
+			if (optionsPassed.hasOwnProperty('site')) {
+				options.site = optionsPassed.site;
+			}
+			if (optionsPassed.hasOwnProperty('target')) {
+				options.target = optionsPassed.target;
+			}
+		}
+
+		var anchors = document.querySelectorAll( options.target + ' a' );
+		for(var i = 0; i < anchors.length; ++i) {
+			var myhref = anchors[i].href;
+			if( myhref && ( (myhref.indexOf( "http://" ) !== -1 || myhref.indexOf( "https://" ) !== -1) && myhref.indexOf( options.site ) === -1 ) ) {
+				anchors[i].target = "_blank";
+			}
+		};
+	};
+
+}());
