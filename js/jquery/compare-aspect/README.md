61cc19533c40d467865c28b9e252fa159f6e05b1
gagechoat <gagechoat@gmail.com>
2018-06-14 08:41:08 -0700

    use outer dimensions for calculating comparer aspect ratio, create inner dimensions option, debounce, remove version number from readme

--- js/jquery/compare-aspect/README.md
+++ js/jquery/compare-aspect/README.md
@@ -1,7 +1,5 @@
 # Harmonic Aspect Ratio Compare jQuery plugin
 
-### Version: 1.1
-
 Adds class to DOM element indicating if its aspect ratio is taller or wider than the provided ratio. This plugin allows video and image elements within a container having a variable aspect ratio to always either cover or be contained within that container via CSS.
 
 https://github.com/harmonicnw/snippets/tree/master/js/jquery/compare-aspect
--- js/jquery/compare-aspect/jquery.hmc-compare-aspect.js
+++ js/jquery/compare-aspect/jquery.hmc-compare-aspect.js
@@ -3,13 +3,14 @@
 	/*
 	Harmonic Aspect Ratio Compare jQuery plugin
   Adds class to DOM element indicating if its aspect ratio is taller or wider than the provided ratio. This plugin allows video and image elements within a container having a variable aspect ratio to always either cover or be contained within that container via CSS.
-	Version: 1.2.1
+	Version: 1.3.0
 	https://github.com/harmonicnw/snippets/tree/master/js/jquery/compare-aspect
 
 	Options:
 	{
 		ratio : ( required | number | default = 1 ) child aspect ratio
 		onComplete : ( function | default = false ) function to run after comparison
+		innerDimensions : ( boolean | default = false ) use height() and width() to calculate aspect ratio of container rather than outerHeight() and outerWidth() which includes padding and borders
 	}
 
 	Usage:
@@ -24,32 +25,61 @@
 	$.fn.extend({
 		hmcCompareAspect: function( optionsPassed ) {
 			var targets = $(this);
+      var ticking = false;
 
 			// set initial parameters
 			var options = {
-				onComplete : false,
-				ratio : 1
+				onComplete: false,
+        innerDimensions: false,
+				ratio: 1
 			};
 
 			$.extend( options, optionsPassed );
 
+      function update() {
+        ticking = false;
+        targets.hmcCAGo( options );
+      }
+
 			$(window).resize( function() {
-				targets.hmcCAGo( options );
+        if (!ticking) {
+          requestAnimationFrame(update);
+        }
+        ticking = true;
 			});
 
-			targets.hmcCAGo( options );
+      update();
 		},
 		hmcCAGo: function( options ) {
-      $(this).removeClass('aspect-compare-taller aspect-compare-wider aspect-compare-equal aspect-compare-loading');
-			var myAspectRatio = $(this).width() / $(this).height();
-      if (myAspectRatio < options.ratio) {
-        $(this).addClass('aspect-compare-taller');
-      } else if (myAspectRatio > options.ratio) {
-        $(this).addClass('aspect-compare-wider');
-      } else {
-        $(this).addClass('aspect-compare-equal');
-      }
-			if (options.onComplete) {
+      $(this).each(function(){
+
+        $(this).removeClass('aspect-compare-taller aspect-compare-wider aspect-compare-equal aspect-compare-loading');
+
+        var width, height;
+
+        if (options.innerDimensions) {
+          width = $(this).width();
+          height = $(this).height();
+        } else {
+          width = $(this).outerWidth();
+          height = $(this).outerHeight();
+        }
+
+        // return if height or width are 0
+        if (width == 0 || height == 0) return;
+
+  			var myAspectRatio = width / height;
+
+        if (myAspectRatio < options.ratio) {
+          $(this).addClass('aspect-compare-taller');
+        } else if (myAspectRatio > options.ratio) {
+          $(this).addClass('aspect-compare-wider');
+        } else {
+          $(this).addClass('aspect-compare-equal');
+        }
+      });
+
+      if (options.onComplete) {
         options.onComplete();
       }
 		}
