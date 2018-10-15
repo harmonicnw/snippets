82c7c419412df5b88ea0238ce49ecf89e87a9835
Gage Pacifera <gagechoat@gmail.com>
2018-10-15 11:27:29 -0700

    fix issue with instantiating multiple addresses

--- js/jquery/obscure-email/jquery.hmc-obscure.js
+++ js/jquery/obscure-email/jquery.hmc-obscure.js
@@ -4,9 +4,9 @@
 
 	/*
 	Harmonic Obscure Email jQuery Plugin
-	Version: 1.2.2
+	Version: 1.2.3
 	https://github.com/harmonicnw/snippets/tree/master/js/jquery/obscure-email
-	
+
 	Options:
 	{
 		emailArray : ( required | array ) series of strings that compromise email address in reverse order
@@ -14,7 +14,7 @@
 		linkType : ( optional | "mailto" or "onclick" or "none" | default = "mailto" ) mailto sets link as mailto, onclick sets as a javascript onclick, anything else does not add a link
 		textObj : (optional | jQuery collection | default = $(this) ) if set, applies text string to an object within the link object
 	}
-	
+
 	Usage:
 	$('a.email-link').hmcObscureEmail({
 		emailArray : [
@@ -23,12 +23,13 @@
 			'my_addres'
 		]
 	});
-	
+
 	To do:
 	 - add option for including email address into text like "Send email to %email% now!"
 	 - add options for subject and body strings
+   - obscure telephone numbers
 	*/
-	
+
 	$.fn.hmcObscureEmail = function( optionsPassed ) {
 		var targets = $(this);
 
@@ -38,22 +39,22 @@
 			text: false,
 			textObj: $()
 		};
-		
+
 		$.extend( options, optionsPassed );
-		
+
 		// create email string
 		var emailArray = options.emailArray.reverse();
 		var email = emailArray.join('');
-		
+
 		// select textObj if not set
 		if ( options.textObj.length === 0 ) {
 			options.textObj = targets;
 		}
-		
+
 		// add mailto links and link text
 		$(this).each( function() {
 			if ( options.linkType === 'mailto' ) {
-				$(this).attr('href', 'mailto:' + email);	
+				$(this).attr('href', 'mailto:' + email);
 			} else if ( options.linkType === 'onclick' ) {
 				$(this).click( function(e) {
 					window.location = 'mailto:' + email;
@@ -61,14 +62,13 @@
 					$(this).blur();
 				});
 			}
-			
+
 			if ( options.text ) {
-				options.textObj.html( options.text );	// if text option is set, set to text
-			} else if ( options.textObj.html() === '' ) {
-				options.textObj.text( email );	// if DOM object is empty, set to email
+				$(this).html( options.text );	// if text option is set, set to text
+			} else if ( $(this).html() === '' ) {
+				$(this).text( email );	// if DOM object is empty, set to email
 			}	// else don't touch.
 		});
 	};
-		
-}( jQuery ));
 
+}( jQuery ));
